7eb65d1ace751f9dfaf030f17971f5e9e35dacfe
Webhortiz <advancoplanet@gmail.com>
2021-01-16 11:44:24 +0100

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-body">
+                    <div class="text-center">
+                        <h3>Imago</h3>
+                        <p>Online Image Repository</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+        
+                                <div class="form-group">
+                                    <label for="email" >{{ __('E-Mail Address') }}</label>
+        
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror rounded-0" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="password">{{ __('Password') }}</label>
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror rounded-0" name="password" required autocomplete="current-password">
+        
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+        
+                               
+        
+                                <div class="form-group">
+                                        <button type="submit" class="btn btn-primary btn-block">
+                                            {{ __('Login') }}
+                                        </button>
+                                        
+                                        @if (Route::has('password.request'))
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                        @endif
+                                    
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
